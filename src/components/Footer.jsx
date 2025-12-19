7e3e747fdef741016c2e2707201a1add340d56ce
Pranav <yeolepranav13@example.com>
2025-12-19 21:08:48 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
             { name: "About", id: "about" },
             { name: "Skills", id: "skills" },
             { name: "Experience", id: "experience" },
-            { name: "Projects", id: "projects" },
+            { name: "Work", id: "projects" },
             { name: "Education", id: "education" },
           ].map((item, index) => (
             <button
